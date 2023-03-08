d8e67f472e4743a21e356cf513ebeefbebe7f951
“Francesca <dascanio.francesca@gmail.com>
2023-03-08 15:08:54 +0100

    Aggiunta rotta per header -> chi siamo // contatti

--- resources/views/hp.blade.php
+++ resources/views/hp.blade.php
@@ -5,10 +5,51 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Laravel Primi Passi</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.4.1/dist/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
     </head>
     <body>
-        <h1>
-            Hello World!
-        </h1>
+        <header style="background-color: lightblue;">
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <nav>
+                            <ul class="d-flex justify-content-between" style="list-style: none; padding-left: 0px; margin-top: 1rem;">
+                                <li>
+                                    <a href="#" style="font-weight: bold; color: black; text-decoration: none;">
+                                        Hello World!
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="http://127.0.0.1:8000/chisiamo" target="_blank" style="color: black; text-decoration: none;">
+                                        Chi siamo
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="http://127.0.0.1:8000/contatti" target="_blank" style="color: black; text-decoration: none;">
+                                        Contatti
+                                    </a>
+                                </li>
+                                <li>
+                                    <a href="#" style="color: black; text-decoration: none;">
+                                        Progetti
+                                    </a>
+                                </li>
+                            </ul>
+                        </nav>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <main>
+            <div class="container">
+                <div class="row">
+                    <div class="col">
+                        <h1 style="text-align: center; color: blue; font-size: 4rem; margin-top: 3rem; font-weight: bold;">
+                            Hello World!
+                        </h1>
+                    </div>
+                </div>
+            </div>
+        </main>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,12 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('hp');
 });
+
+Route::get('/chisiamo', function () {
+    return view('chisiamo');
+});
+
+
+Route::get('/contatti', function () {
+    return view('contatti');
+});
